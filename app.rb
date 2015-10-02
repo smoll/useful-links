750e95b8406b1c384b66234d8dcf0b93bcef189e
smoll <smollah@theorchard.com>
2015-10-02 16:18:27 -0400

    Fix archival on home page; TODO: figure out why the edit page isn't saving correct archival status

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require "data_mapper"
 require "json"
 require "sinatra"
@@ -27,7 +28,34 @@ post "/" do
   redirect "/"
 end
 
-get "/links" do
-  content_type "application/json"
-  Link.all(order: :id.desc).to_json
+get "/:id" do
+  @link = Link.get params[:id]
+  @title = "Edit link ##{params[:id]}"
+  erb :edit
+end
+
+put "/:id" do
+  link = Link.get params[:id]
+  link.url = params[:url]
+  link.description = params[:description]
+  link.archived = params[:archived] ? 1 : 0
+  link.created_at = Time.now
+  link.updated_at = Time.now
+  logger.info "Will link be archived? #{link.archived}"
+  link.save
+  redirect "/"
+end
+
+get "/:id/archived" do
+  link = Link.get params[:id]
+  link.archived = link.archived ? 0 : 1 # flip it
+  link.updated_at = Time.now
+  link.save
+  redirect "/"
+end
+
+get "/:id/delete" do
+  link = Link.get params[:id]
+  link.destroy
+  redirect "/"
 end
